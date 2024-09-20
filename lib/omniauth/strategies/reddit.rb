5ee381a8a0b36d7adca36ab7cd4ee015accdd783
Mirko Mignini <mirko.mignini@gmail.com>
2024-09-20 14:18:13 +0200

    Update reddit.rb

--- lib/omniauth/strategies/reddit.rb
+++ lib/omniauth/strategies/reddit.rb
@@ -9,36 +9,30 @@ module OmniAuth
       option :authorize_options, [:scope, :duration]
 
       option :client_options, {
-        authorize_url: 'https://www.reddit.com/api/v1/authorize',
-        site: 'https://www.reddit.com'
+        site: 'https://www.reddit.com',
+        authorize_url: '/api/v1/authorize',
+        token_url: '/api/v1/access_token'
       }
 
       uid { raw_info['id'] }
 
       info do
         {
-          name: raw_info['name']
+          nickname: raw_info['name'],
+          email: raw_info['email'],
+          image: raw_info['icon_img'],
+          verified: raw_info['verified']
         }
       end
 
       extra do
-        {'raw_info' => raw_info}
+        {
+          raw_info: raw_info
+        }
       end
+
       def raw_info
-        @raw_info ||= access_token.get('/api/v1/me').parsed || {}
-      end
-
-      def build_access_token
-        options.token_params.merge!(:headers => {'Authorization' => basic_auth_header })
-        super
-      end
-
-      def basic_auth_header
-        "Basic " + Base64.strict_encode64("#{options[:client_id]}:#{options[:client_secret]}")
-      end
-      
-      def callback_url
-        options[:redirect_uri] || (full_host + script_name + callback_path)
+        @raw_info ||= access_token.get('/api/v1/me').parsed
       end
 
     end
